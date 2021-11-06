15ae607f10b9822db4f9b9555bee1cbfebe4894f
WinstonLam <winston.lam@outlook.com>
2021-11-06 10:07:58 +0100

    finished check & queue

--- maze_solver/maze_solver_dfs.c
+++ maze_solver/maze_solver_dfs.c
@@ -8,29 +8,46 @@
 #define NOT_FOUND -1
 #define ERROR -2
 
+int row, col;
 /* Solves the maze m.
  * Returns the length of the path if a path is found.
  * Returns NOT_FOUND if no path is found and ERROR if an error occured.
  */
-int dfs_solve(struct maze *m) {
-    /* ... SOME CODE MISSING HERE ... */
+int dfs_solve(struct maze *m)
+{
+    if (maze_start(m, &row, &col) == maze_destination(m, &row, &col))
+    {
+    }
+
+    struct stack *s = stack_init(1000);
+
+    // maze[start point] hoe vind ik deze
+    // hoe loop ik vervolgens in de maze?
+
+    // hoe geef ik aan waar ik in de maze ben, hoe define ik de rows en columms
+    // hoe print ik dingen verveolgens naar de file
 }
 
-int main(void) {
+int main(void)
+{
     /* read maze */
     struct maze *m = maze_read();
-    if (!m) {
+    if (!m)
+    {
         printf("Error reading maze\n");
         return 1;
     }
 
     /* solve maze */
     int path_length = dfs_solve(m);
-    if (path_length == ERROR) {
+    if (path_length == ERROR)
+    {
         printf("dfs failed\n");
         maze_cleanup(m);
         return 1;
-    } else if (path_length == NOT_FOUND) {
+    }
+    else if (path_length == NOT_FOUND)
+    {
         printf("no path found from start to destination\n");
         maze_cleanup(m);
         return 1;
