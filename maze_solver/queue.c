18f7b241fb53672a8669a2a11b3b8450542fb579
WinstonLam <winston.lam@outlook.com>
2021-11-06 10:08:18 +0100

    finished queue & stack

--- maze_solver/queue.c
+++ maze_solver/queue.c
@@ -5,9 +5,9 @@
 
 struct queue
 {
-    size_t capacity;
+    size_t capacity, front, rear, used;
     int *arr;
-    int push_count, pop_count, front, rear;
+    int push_count, pop_count, max_elem;
 };
 
 struct queue *queue_init(size_t capacity)
@@ -19,37 +19,84 @@ struct queue *queue_init(size_t capacity)
     queue->rear = -1;
     queue->push_count = 0;
     queue->pop_count = 0;
+    queue->max_elem = 0;
+    queue->used = 0;
     queue->capacity = capacity;
     queue->arr = (int *)malloc(capacity * sizeof(int));
     if (queue->arr == NULL)
+    {
+        free(queue);
         return NULL;
+    }
     return queue;
+}
+/* Check if the queue is full, by checking if 
+the top index = capacity, return 1 if full 0 if not.*/
+static int is_full(const struct queue *q)
+{
+    if ((size_t)q->used == (q->capacity))
+    {
+        return 1;
+    }
+    return 0;
 }
 
 void queue_cleanup(struct queue *q)
 {
-    free(q);
+    if (q == NULL)
+    {
+        return;
+    }
     free(q->arr);
+    free(q);
     return;
 }
 
 void queue_stats(const struct queue *q)
 {
-    /* ... SOME CODE MISSING HERE ... */
+    fprintf(stderr, "'stats' %d %d %d", q->push_count, q->pop_count, q->max_elem);
 }
 
 int queue_push(struct queue *q, int e)
 {
-    if (is_full(q) == 1)
+    if (q == NULL)
+    {
         return 1;
-    q->arr[++q->rear] = e;
+    }
+    else if (is_full(q) == 1)
+    {
+        return 1;
+    }
+    else if ((queue_empty(q)) == 1)
+    {
+        q->front = q->rear = 0;
+    }
+    q->arr[q->rear] = e;
+    q->rear = ((size_t)q->rear + 1) % q->capacity;
     q->push_count += 1;
+    q->used += 1;
+    q->max_elem += 1;
     return 0;
 }
 
 int queue_pop(struct queue *q)
 {
-    /* ... SOME CODE MISSING HERE ... */
+    if (q == NULL)
+    {
+        return -1;
+    }
+    int temp = q->arr[q->front];
+
+    if (queue_empty(q) == 0)
+    {
+        q->front = ((size_t)q->front + 1) % q->capacity;
+        q->used -= 1;
+        return temp;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
 int queue_peek(const struct queue *q)
@@ -61,22 +108,18 @@ int queue_peek(const struct queue *q)
 
 int queue_empty(const struct queue *q)
 {
-    if (q->rear == -1)
-        return 1;
-    else if (q->rear >= 0)
+    if (q == NULL)
+    {
+        return -1;
+    }
+    else if (q->used > 0)
+    {
         return 0;
-    return -1;
-}
-/* Check if the queue is full, by checking if 
-the top index = capacity, return 1 if full 0 if not.*/
-int is_full(const struct queue *q)
-{
-    if (q->rear >= 0 && (size_t)q->rear == q->capacity)
-        return 0;
+    }
     return 1;
 }
 
 size_t queue_size(const struct queue *q)
 {
-    return (size_t)q->rear + 1;
+    return q->used;
 }
--- maze_solver/stack.c
+++ maze_solver/stack.c
@@ -6,54 +6,65 @@ struct stack
 {
     size_t capacity;
     int *arr;
-    int top, push_count, pop_count;
+    int top, push_count, pop_count, max_elem;
 };
 
 struct stack *stack_init(size_t capacity)
 {
-    struct stack *stack = (struct stack *)malloc(sizeof(struct stack));
+    struct stack *stack = malloc(sizeof(struct stack));
     if (stack == NULL)
         return NULL;
     stack->capacity = capacity;
     stack->top = -1;
     stack->push_count = 0;
     stack->pop_count = 0;
-    stack->arr = (int *)malloc(stack->capacity * sizeof(int));
+    stack->max_elem = 0;
+    stack->arr = malloc(stack->capacity * sizeof(int));
     if (stack->arr == NULL)
+    {
+        free(stack);
         return NULL;
+    }
     return stack;
 }
 
 /* Check if the stack is full, by checking if 
 the top index = capacity, return 1 if full 0 if not.*/
-int is_full(const struct stack *s)
+static int is_full(const struct stack *s)
 {
-    if (s->top >= 0 && (size_t)s->top == s->capacity)
-        return 0;
-    return 1;
+    if (s->top >= 0 && (size_t)s->top == (s->capacity - 1))
+        return 1;
+    return 0;
 }
 
 void stack_cleanup(struct stack *s)
 {
-    free(s);
+    if (s == NULL)
+    {
+        return;
+    }
     free(s->arr);
+    free(s);
     return;
 }
 
 void stack_stats(const struct stack *s)
 {
-    printf("stats");
-    /*   fprintf("%d,", s->push_count);
-    fprintf("%d,", s->pop_count); */
+    fprintf(stderr, "'stats' %d %d %d", s->push_count, s->pop_count, s->max_elem);
 }
 
 int stack_push(struct stack *s, int c)
 {
-    // wat als we pushen naar eens stack die niet bestaat
-    if (is_full(s) == 1)
+    if (s == NULL)
+    {
         return 1;
+    }
+    else if (is_full(s) == 1)
+        return 1;
+
     s->arr[++s->top] = c;
     s->push_count += 1;
+    s->max_elem += 1;
     return 0;
 }
 
@@ -69,7 +80,6 @@ int stack_pop(struct stack *s)
 
 int stack_peek(const struct stack *s)
 {
-    // wat te doen bij een lege stack
     if (stack_empty(s) != 0)
         return -1;
     return s->arr[s->top];
@@ -77,7 +87,11 @@ int stack_peek(const struct stack *s)
 
 int stack_empty(const struct stack *s)
 {
-    if (s->top == -1)
+    if (s == NULL)
+    {
+        return -1;
+    }
+    else if (s->top == -1)
     {
         return 1;
     }
