bfdb73199e29ffa44a2ce6c2b87e67fe8d7477de
WinstonLam <winston.lam@outlook.com>
2021-12-04 13:05:50 +0100

    finish percolate

--- heaps/heap.c
+++ heaps/heap.c
@@ -44,28 +44,30 @@ int prioq_cleanup(prioq *q, void free_func(void *)) {
 
 static int heap_insert(struct heap *h, void *p) {
     if (h == NULL) return -1;
-
-    long size = array_size(h->array);
-    long parent = (size - 1) / 2;
-    long child = size + 1;
-
     // Append new node to array.
     if (array_append(h->array, p) == -1) return -1;
-
+    
+    long size = array_size(h->array) - 1;
+    long parent = (size - 1) / 2;
+    
+    if (size == 0) return 0;
+    
     // If node has a parent, check if it needs to be percolated up.
-    while (parent > 0){
+    while (parent >= 0){
+       
         // If node is greater than the parent.
-        if (parent > 0 || h->compare(p, array_get(h->array, parent)) > 0){
-        
+        if (h->compare(p, array_get(h->array, parent)) > 0){
+     
             // Copy parent to end.
-            if (array_set(h->array, child, array_get(h->array, parent)) == -1) return -1;
+            if (array_set(h->array, size, array_get(h->array, parent)) == -1) return -1;
+          
             // Set new child at the index of parent.
             if(array_set(h->array, parent, p) == -1) return -1;
+    
 
         }
-        // If node is lesser thant the parent. Then simply return.
-        if (parent <= 0){
-            if (array_append(h->array, p) == -1) return -1;
+        // If node is lesser than the parent. Then simply return.
+        if (h->compare(p, array_get(h->array, parent)) <= 0){
             return 0;
         }
         // Move parent to upper parent to go up the heap.
@@ -80,13 +82,18 @@ int prioq_insert(prioq *q, void *p) {
 }
 
 static void *heap_pop(struct heap *h) {
+   
     if (h == NULL) return NULL;
-
+    if (array_size(h->array) == 0){
+        return NULL;
+    }
     // If heap only has one element.
     if (array_size(h->array) == 1){
-        // Pop the first index.
-        free(array_get(h->array, 0));
-        return h;
+        // Store the element in array for returning.
+        void *elem = array_get(h->array,0);
+        // Pop the only index.
+        array_pop(h->array);
+        return elem;
     }
 
     // Pop last element in array and replace it with the front.
@@ -136,9 +143,35 @@ static void *heap_pop(struct heap *h) {
         }
 
     }
-    return h;
+    return array_get(h->array, 0);
 }
 
 void *prioq_pop(prioq *q) {
     return heap_pop(q);
 }
+
+
+/* 
+  int int_compare(const void *a, const void *b) {
+    int x = *((const int *) a);
+    int y = *((const int *) b);
+
+    return x - y;
+}
+void main(){
+
+    prioq *p = prioq_init(int_compare);
+
+    int values[3] = { 2, 5, 2};
+
+    for (int i = 0; i < 3; i++){
+        prioq_insert(p, values + i);
+    }
+    for (int i = 0; i < 3; i++){
+        printf("value: %ld\n",*((int *)array_get(p->array, i)));
+    } 
+
+
+}
+
+ */
