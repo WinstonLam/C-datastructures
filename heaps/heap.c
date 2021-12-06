52cb3ba4282bd3751acbc7351e85a8bc76f5b2ba
WinstonLam <winston.lam@outlook.com>
2021-12-06 13:59:42 +0100

    Stuck on insertion

--- heaps/heap.c
+++ heaps/heap.c
@@ -44,34 +44,42 @@ int prioq_cleanup(prioq *q, void free_func(void *)) {
 
 static int heap_insert(struct heap *h, void *p) {
     if (h == NULL) return -1;
+
     // Append new node to array.
     if (array_append(h->array, p) == -1) return -1;
     
-    long size = array_size(h->array) - 1;
-    long parent = (size - 1) / 2;
     
-    if (size == 0) return 0;
+    long current = array_size(h->array) - 1;
+    long parent = (current - 1) / 2;
     
-    // If node has a parent, check if it needs to be percolated up.
-    while (parent >= 0){
-       
-        // If node is greater than the parent.
-        if (h->compare(p, array_get(h->array, parent)) > 0){
-     
-            // Copy parent to end.
-            if (array_set(h->array, size, array_get(h->array, parent)) == -1) return -1;
-          
-            // Set new child at the index of parent.
-            if(array_set(h->array, parent, p) == -1) return -1;
-    
+    // If heap was empty simply return.
+    if (current == 0) return 0;
 
-        }
-        // If node is lesser than the parent. Then simply return.
-        if (h->compare(p, array_get(h->array, parent)) <= 0){
+    // If node has a parent, check if it needs to be percolated up.
+    while (current >= 0){
+       
+        // If node is greater than the parent. Then simply return.
+        if (h->compare(p, array_get(h->array, parent)) >= 0){
             return 0;
         }
+        // If node is lesser than the parent.
+        if (h->compare(p, array_get(h->array, parent)) < 0){
+            printf("parent: %ld\n", *((int *) array_get(h->array, parent)));
+            printf("current: %ld\n", *((int *) array_get(h->array, current)));
+
+      
+            // Copy parent to end.
+            if (array_set(h->array, current, array_get(h->array, parent)) == -1) return -1;
+          
+            // Set new node at the index of parent.
+            if(array_set(h->array, parent, p) == -1) return -1;
+
+        }
         // Move parent to upper parent to go up the heap.
-        parent = parent;
+        current = parent;
+        parent = (parent - 1) / 2;
+
+        
     }
     return 0;
 }
@@ -87,21 +95,22 @@ static void *heap_pop(struct heap *h) {
     if (array_size(h->array) == 0){
         return NULL;
     }
+    // Store the element in array for returning.
+    void *elem = array_get(h->array,0);
+    
     // If heap only has one element.
     if (array_size(h->array) == 1){
-        // Store the element in array for returning.
-        void *elem = array_get(h->array,0);
         // Pop the only index.
         array_pop(h->array);
         return elem;
     }
-
+    
     // Pop last element in array and replace it with the front.
     array_set(h->array, 0, array_pop(h->array));
-
-    long curr_index = 1;
-    long l_index = 2;
-    long r_index = 3;
+  
+    long curr_index = 0;
+    long l_index = 1;
+    long r_index = 2;
     
     for(long i = 0; i < array_size(h->array); i++){
 
@@ -109,15 +118,15 @@ static void *heap_pop(struct heap *h) {
         void *current = array_get(h->array, curr_index);
         void *left = array_get(h->array, l_index);
         void *right = array_get(h->array, r_index);
-
-        // If node is at final spot, not smaller than left child and not larger than right child.
-        if (h->compare(current, left) > 0 
-        && h->compare(current, right) < 0){
+           
+        // If node is at final spot, not larger than left child or right child.
+        if (right != NULL && left != NULL && h->compare(current, left) <= 0 
+        && h->compare(current, right) <= 0){
             break;
         }
-
-        // Check if node is smaller than left child.
-        if (h->compare(current, left) < 0 ){
+ 
+        // Check if node is larger than left child.
+        if (left != NULL && h->compare(current, left) > 0){
             // Set given node on position of its child.
             if (array_set(h->array, l_index, current) == -1) return NULL;
             // Set child on postion of given node.
@@ -130,7 +139,7 @@ static void *heap_pop(struct heap *h) {
         }
 
         // Check if node is larger than right child.
-        if (h->compare(current, right) > 0 ){
+        if (right != NULL && h->compare(current, right) > 0 ){
             // Set given node on position of its child.
             if (array_set(h->array, r_index, current) == -1) return NULL;
             // Set child on postion of given node.
@@ -141,9 +150,9 @@ static void *heap_pop(struct heap *h) {
             l_index = curr_index * 2;
             r_index = l_index + 1;
         }
-
+        break;
     }
-    return array_get(h->array, 0);
+    return elem;
 }
 
 void *prioq_pop(prioq *q) {
@@ -151,8 +160,7 @@ void *prioq_pop(prioq *q) {
 }
 
 
-/* 
-  int int_compare(const void *a, const void *b) {
+    int int_compare(const void *a, const void *b) {
     int x = *((const int *) a);
     int y = *((const int *) b);
 
@@ -162,16 +170,17 @@ void main(){
 
     prioq *p = prioq_init(int_compare);
 
-    int values[3] = { 2, 5, 2};
+    int values[10] = { 5, 7, 2, 4, 0, 9, 12, 13, 28, 1 };
+    int expected[10] = { 0, 1, 2, 4, 5, 7, 9, 12, 13, 28 };
 
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 10; i++){
         prioq_insert(p, values + i);
+       
     }
-    for (int i = 0; i < 3; i++){
-        printf("value: %ld\n",*((int *)array_get(p->array, i)));
+    for (long i = 0; i < 10; i++){
+       printf("pop: %ld\n", *((int *) array_get(p->array, i))); 
     } 
 
 
 }
-
- */
+ 
