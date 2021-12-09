3c448a38ffc2c8b241f91beb4147011c4e3d33ac
WinstonLam <winston.lam@outlook.com>
2021-12-09 22:18:48 +0100

    at main

--- heaps/heap.c
+++ heaps/heap.c
@@ -1,3 +1,18 @@
+/* Name: Winston Lam
+ * Student ID: 11844078
+ * Course: Datastructuren
+ * Institution: University of Amsterdam
+ *
+ * heap.c:
+ * DESCRIPION:
+ *    In this file all the internal functions of 
+ *    the struct heap are defined. Function such as
+ *    Initialization, getting certain values, popping values,
+ *    cleaning the array and inserting values.
+ * USAGE:
+ *    This File can be used by including the header file heap.c
+ *    And then calling the functions by passing the right variables.
+ */
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,18 +21,18 @@
 #include "prioq.h"
 
 static struct heap *heap_init(int (*compare)(const void *, const void *)) {
+    // Initialize heap and perform malloc check.
     struct heap *h = malloc(sizeof(struct heap));
     if (h == NULL) return NULL;
     
+    // Intialize array and perform malloc check.
     struct array* array = array_init(10);
     if (array == NULL){
         free(h);
         return NULL;
     }
+    // Set array and the compare function.
     h->array = array;
-    /* Store the function pointer compare argument in the struct here.
-       If the struct pointer is called 'h' the assignment is:
-       h->compare = compare; */
     h->compare = compare;
     return h;
 }
@@ -27,13 +42,14 @@ prioq *prioq_init(int (*compare)(const void *, const void *)) {
 }
 
 long int prioq_size(prioq *q) {
+    // Return queue size if queue is not NULL.
     if (q == NULL) return 0;
     return array_size(q->array);
 }
 
 static int heap_cleanup(struct heap *h, void free_func(void *)) {
     if (h == NULL) return 0;
-    
+    // Cleanup the array using the free func then clean up h.
     array_cleanup(h->array, free_func);
     free(h);
     return 0;
@@ -51,6 +67,7 @@ static int heap_insert(struct heap *h, void *p) {
     // Append new node to array.
     if (array_append(h->array, p) == -1) return -1;
     
+    // Store current and parent index for swapping.
     long current = array_size(h->array) - 1;
     long parent = (current - 1) / 2;
     
@@ -61,24 +78,23 @@ static int heap_insert(struct heap *h, void *p) {
     while (current >= 0){
        
         // If node is greater than the parent. Then simply return.
-        if (h->compare(p, array_get(h->array, parent)) >= 0){
+        if (h->compare(p, array_get(h->array, parent)) >= 0) {
             return 0;
         }
         // If node is lesser than the parent.
-        if (h->compare(p, array_get(h->array, parent)) < 0){
+        if (h->compare(p, array_get(h->array, parent)) < 0) {
 
-            // Copy parent to end.
+            // Copy parent to current.
             if (array_set(h->array, current, array_get(h->array, parent)) == -1) return -1;
           
             // Set new node at the index of parent.
             if(array_set(h->array, parent, p) == -1) return -1;
 
         }
-        // Move parent to upper parent to go up the heap.
+        /* Move current to parent index and
+        parent to upper parent to go up the heap. */
         current = parent;
         parent = (parent - 1) / 2;
-
-        
     }
     return 0;
 }
@@ -87,26 +103,34 @@ int prioq_insert(prioq *q, void *p) {
     return heap_insert(q, p);
 
 }
-
-static long index_child(struct heap *h, long index){
+/* This function returns the index of the 
+smallest child of the given index
+on failure it will return -1.*/
+static long index_child(struct heap *h, long index) {
     if (h == NULL) return -1;
-
+    // Set left and right child index.
     long l_index = 1;
     long r_index = 2;
-
+    
+    // If Index is not at beginning adjust left and right child.
     if (index != 0){
         l_index = index * 2 + 1;
         r_index = l_index + 1;
     }
+    // If the index only has one child return.
     if (l_index == array_size(h->array)) return -1;
+
+    // Store left and right child in void pointer for comparison.
     void *left = array_get(h->array, l_index);
     void *right = array_get(h->array, r_index);
     
+    // Chech if children exist.
     if (left == NULL && right == NULL) return -1;
     else if (left == NULL) return r_index;
     else if (right == NULL) return l_index;
 
-    if (h->compare(left, right) <= 0){
+    // Compare left and right to determine which index is smallest.
+    if (h->compare(left, right) <= 0) {
             return l_index;
     } else {
             return r_index;
@@ -129,27 +153,32 @@ static void *heap_pop(struct heap *h) {
     array_set(h->array, 0, array_pop(h->array));
 
     // If heap only has one element.
-    if (array_size(h->array) <= 1){
+    if (array_size(h->array) <= 1) {
         return elem;
     }
-
+    // Store current and parent index for swapping.
     long curr_index = 0;
     long child_index = index_child(h, curr_index);
+
+    // Store current and smallest child in void pointer for comparison.
     void *current = array_get(h->array, curr_index);
     void *child = array_get(h->array, child_index);
 
-
-    while (h->compare(current, child) > 0){
-
+    // Compare current and child and swap if needed.
+    while (h->compare(current, child) > 0) {
+        // Set child at current index.
         if (array_set(h->array, curr_index, child) == -1) return NULL;
+        // Set current at child index.
+        if (array_set(h->array, child_index, current) == -1) return NULL;
 
-        if (array_set(h->array, child_index, current) == -1 ) return NULL;
-
+        // Update current and child index.
         curr_index = child_index;
         child_index = index_child(h, curr_index);
 
+        // If last child index is reached break out of loop.
         if (child_index < 0) break;
 
+        // Update current and child node pointers.
         current = array_get(h->array, curr_index);
         child = array_get(h->array, child_index);
     }
--- heaps/main copy 1.c
+++ heaps/main copy 1.c
@@ -1,25 +1,10 @@
-/* Name: Winston Lam
- * Student ID: 11844078
- * Course: Datastructuren
- * Institution: University of Amsterdam
- *
- * main.c:
- * DESCRIPION:
- *    This file gets the patients from stdin and adds them to
- *    a priorty queue. Then the patients will be printed followed 
- *    by . This file also is able to sort patients not only based on
- *    name but also age in the order that they will be treated. Once a 
- *    patient is treated they will be removed from the queue. 
- * USAGE:
- *    This File can be used by running it with a txt file or writing
- *    on the stdin.
- */
 #include <fcntl.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include "prioq.h"
 
 #define BUF_SIZE 1024
@@ -29,11 +14,13 @@ static char buf[BUF_SIZE];
 struct config {
     /* You can ignore these options until/unless you implement the */
     /* bonus features. */
+
     /* Set to 1 if -y is specified, 0 otherwise. */
     int year;
 };
 
 static int parse_options(struct config *cfg, int argc, char *argv[]);
+
 typedef struct {
     char *name;
     int age;
@@ -45,43 +32,37 @@ static int compare_patient_name(const void *a, const void *b) {
 }
 
 static int compare_patient_age(const void *a, const void *b) {
-    // If age is the same order on name
+    
     if ((((const patient_t *) a)->age) 
     - (((const patient_t *) b)->age) == 0){
         return strcmp(((const patient_t *) a)->name, ((const patient_t *) b)->name);
     }
-
-    // Return difference between the age.
     return (((const patient_t *) a)->age) - (((const patient_t *) b)->age);
     
 }
-/* This function takes prioq as input and determines 
-what should be printed based on int value's. if it fails
-it will return 0. If patient takes longer to treat it will return 2.*/
 static int print_patients(struct heap *q){
     if (q == NULL) return 0;
+    
     if ((array_size(q->array)) == 0) return 2;
     patient_t *patient_top = array_get(q->array, 0);
-    
+
     if (patient_top->duration && patient_top->duration > 1){
         patient_top->duration -= 1;
         return 2;
     }
-    // Update the prioq after popping the top item.
-    prioq_pop(q);
+    
     return 1;
     }
 
-/* This function takes care of setting all the info 
-from stdin to the patient_t struct. */
 static patient_t *set_info(char *s){
   
     // Malloc token and name_cpy and pefrom malloc checks.
     char *token = strtok(s, " ");
     char *name_cpy = malloc((strlen(token) + 1) * sizeof(char));
-    if (name_cpy == NULL) {
+    if (name_cpy == NULL){
         return NULL;
     }
+
     // Copy token to name_cpy.
     strcpy(name_cpy, token);
     
@@ -89,32 +70,35 @@ static patient_t *set_info(char *s){
     char *data[3];
     data[2] = NULL;
     int i = 0;
-    while (token != NULL) {
+    while (token != NULL){
         data[i++] = token;
         token = strtok(NULL, " ");
     }
+
     // Create pointer to struct patient_t for insertion. 
     patient_t *patient_n = malloc(sizeof(patient_t));
-    if (patient_n == NULL) {
+    if (patient_n == NULL){
         free(name_cpy);
     }
-
     // Set patient name and age.
     patient_n->name = name_cpy;
     patient_n->age = atoi(data[1]);
-
     // If the duration is given set this aswell.
     if (data[2] != NULL) {
         patient_n->duration = atoi(data[2]);
     }
     return patient_n;
 }   
+
 int main(int argc, char *argv[]) {
+    patient_t *in_treatment = NULL;
     prioq *queue;
     struct config cfg;
+
     if (parse_options(&cfg, argc, argv) != 0) {
         return EXIT_FAILURE;
     }
+
     if (cfg.year) {
         queue = prioq_init(&compare_patient_age);
     } else {
@@ -126,15 +110,16 @@ int main(int argc, char *argv[]) {
         free(queue);
         return EXIT_FAILURE; 
     }
+
     for (int iterations = 0;;) {
         while (1) {
             char *s = fgets(buf, BUF_SIZE, stdin);
             if (s == NULL) {
                 fprintf(stderr, "Unexpected end of file. exiting\n");
-                // Cleanup array on failure
                 array_cleanup(queue->array, NULL);
                 free(queue);
                 return EXIT_FAILURE;
+
             } else if (s[0] == '.') {
                 break;
             }
@@ -147,20 +132,35 @@ int main(int argc, char *argv[]) {
             }
         
             // Insert the patient into the prioq.
-            if (prioq_insert(queue, patient_n) != 0) {
+            if (prioq_insert(queue, patient_n) != 0){
                 free(patient_n);
                 return EXIT_FAILURE;
             } 
+
+        }
+        if (in_treatment != NULL){
+            if(--in_treatment->duration == 0){
+                printf("%s\n", in_treatment->name);
+                in_treatment = NULL;
+                
+            }
+            printf(".\n"); /* End turn. */
+            continue;
         }
         patient_t *patient_n = array_get(queue->array, 0);
+
         int print = print_patients(queue);
 
-        // Decide what to print based on print_patients output.           
         if (print == 0){
             prioq_cleanup(queue, NULL);
             return EXIT_FAILURE;
-        } else if (print == 1){
+        } else if (print == 2) {
+            in_treatment = patient_n;
+        } else {
+
             printf("%s\n", patient_n->name);
+            // Update the prioq after popping the top item.
+            prioq_pop(queue);
 
             // print patient name 
             free(patient_n->name);
@@ -169,12 +169,10 @@ int main(int argc, char *argv[]) {
           
         printf(".\n"); /* End turn. */
 
-        // If max hours are reached pop all items and print them.
         if (++iterations == 10) {
             patient_t *p = prioq_pop(queue);
             while(p) {
                 printf("%s\n", p->name);
-
                 free(p->name);
                 free(p);
                 p = prioq_pop(queue);
@@ -185,6 +183,7 @@ int main(int argc, char *argv[]) {
     prioq_cleanup(queue, NULL);
     return EXIT_SUCCESS;
 }
+
 int parse_options(struct config *cfg, int argc, char *argv[]) {
     memset(cfg, 0, sizeof(struct config));
     int c;
